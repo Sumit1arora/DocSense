5cfcf2b8d114cdeb71cdb5d930829fb0f5c343f9
Sumit1arora <sa7984469@gmail.com>
2026-06-22 17:44:40 +0530

    Init

---
+++ src/main/java/com/sumit/docsensex/DocSenseApplication.java
@@ -0,0 +1,13 @@
+package com.sumit.docsensex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocSenseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DocSenseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sumit/docsensex/DocSenseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sumit.docsensex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocSenseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
